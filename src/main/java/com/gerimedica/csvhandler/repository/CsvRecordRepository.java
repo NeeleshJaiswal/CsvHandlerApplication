4dabc50d96558118be54ee131d083e3f73bec90c
Neelesh <46023123+NeeleshJaiswal@users.noreply.github.com>
2025-01-29 22:13:16 +0100

    Delete src/main/java/com/gerimedica/csvhandler directory

--- src/main/java/com/gerimedica/csvhandler/repository/CsvRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gerimedica.csvhandler.repository;
-
-import com.gerimedica.csvhandler.model.CsvRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CsvRecordRepository extends JpaRepository<CsvRecord, String> {
-}
